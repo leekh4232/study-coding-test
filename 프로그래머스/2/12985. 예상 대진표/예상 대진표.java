cd5e3c8c0030a7f4e39dc495e9b5d180057b443e
이광호 <leekh4232@gmail.com>
2025-01-12 03:57:19 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+
+        while (a != b) { // 두 참가자가 만날 때까지 반복
+            a = (a + 1) / 2; // 다음 라운드의 참가자 번호 계산
+            b = (b + 1) / 2; // 다음 라운드의 참가자 번호 계산
+            answer++; // 라운드 수 증가
+        }
+
+        return answer;
+    }
+}
