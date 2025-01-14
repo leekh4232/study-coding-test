5a439f53ef415ab59aff08f0d42a3fa17195613f
이광호 <leekh4232@gmail.com>
2025-01-14 16:21:31 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 3.48 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/README.md
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 나누어 떨어지는 숫자 배열 - 12910 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=python3) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 17.54 ms
+메모리: 13 MB, 시간: 3.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 14일 16:20:35
+2025년 01월 14일 16:21:29
 
 ### 문제 설명
 
--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -1,12 +1,39 @@
+"""
+프로그래머스 12910번 - 나누어 떨어지는 숫자 배열
+
+https://school.programmers.co.kr/learn/courses/30/lessons/12910
+"""
+# solution.py
 def solution(arr, divisor):
-    # 나누어 떨어지는 숫자 필터링
-    result = []
-    for num in arr:
-        if num % divisor == 0:
-            result.append(num)
-    # 결과 정렬
-    result.sort()
+    # [풀이1] 나누어 떨어지는 숫자 필터링
+    # result = []
+
+    # # arr이 [5, 9, 7, 10]일 때 num은 5, 9, 7, 10이 됨
+    # for num in arr:
+    #     # num이 5, 9, 7, 10일 때 divisor로 나누어 떨어지는지 확인
+    #     if num % divisor == 0:
+    #         result.append(num)
+
+    # # 결과 반환, 비어있으면 [-1] 반환
+    # if not result:
+    #     return [-1]
+
+    # # 결과 정렬
+    # result.sort()
+
+    # return result
+
+    # [풀이2] 나누어 떨어지는 숫자 필터링 (람다식 버전)
+    result = [num for num in arr if num % divisor == 0]
     # 결과 반환, 비어있으면 [-1] 반환
     if not result:
         return [-1]
+    # 결과 정렬
+    result.sort()
     return result
+
+if __name__ == '__main__':
+    print(solution([1, 5, 9, 7, 10], 5))   # [5, 10]
+    print(solution([2, 36, 1, 3], 1))   # [1, 2, 3, 36]
+    print(solution([3, 2, 6], 10))      # [-1]
+    print(solution([3, 2, 6], 2))       # [2, 6]
