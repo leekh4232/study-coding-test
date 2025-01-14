b59dbe3a1a8d0758fe350da02289039b0d4818a1
이광호 <leekh4232@gmail.com>
2025-01-14 16:17:54 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 3.18 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자연수 뒤집어 배열로 만들기 - 12932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 3.97 ms
+메모리: 89.7 MB, 시간: 3.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 12일 09:00:05
+2025년 01월 14일 16:17:51
 
 ### 문제 설명
 
--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,5 +1,13 @@
-class Solution {
+/**
+ * 프로그래머스 12932번 - 자연수 뒤집어 배열로 만들기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12932
+ */
+import java.util.Arrays;
+
+public class Solution {
     public int[] solution(long n) {
+        /**
+        // [풀이1] 특정 위치의 원소와 교환할 반대쪽 위치를 계산하는 형태
         char[] c = String.valueOf(n).toCharArray();
         int size = c.length;
         int half = size / 2;
@@ -17,5 +25,16 @@ class Solution {
             answer[i] = c[i] - '0';
         }
         return answer;
+        /*/
+        // [풀이2] StringBuilder 클래스의 reverse() 메소드를 사용하는 형태
+        String str = new StringBuilder(String.valueOf(n)).reverse().toString();
+        return str.chars().map(c -> c - '0').toArray();
+        /**/
+    }
+
+    public static void main(String[] args) {
+        int n = 12345;
+        int[] result = new Solution().solution(n);
+        System.out.println(Arrays.toString(result));
     }
 }
