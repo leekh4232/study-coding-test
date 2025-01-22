4b862e960945e193e38ea1acb6ad35c0e5136ef8
이광호 <leekh4232@gmail.com>
2025-01-22 17:41:30 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.17 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = ''
+    
+    for m in my_string:
+        if m.isupper():
+            answer += m.lower()
+        else:
+            answer += m.upper()
+    
+    return answer
