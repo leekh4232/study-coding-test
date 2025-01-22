728b9d1a0d48674c3292effea1318606ef73b6aa
이광호 <leekh4232@gmail.com>
2025-01-23 05:24:07 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.py
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -1,5 +1,9 @@
 def solution(num1, num2):
+    answer = 0
+    
     if num1 == num2:
-        return 1
+        answer = 1
     else:
-        return -1
+        answer = -1
+    
+    return answer
