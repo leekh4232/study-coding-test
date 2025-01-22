5a7bcdb711129535eb521239a9ae476c98d119fb
이광호 <leekh4232@gmail.com>
2025-01-22 11:52:27 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    
+    for (let i = 1; i < n * 6; i++) {
+        if (6 * i % n == 0) {
+            answer = i;
+            break;
+        }
+    }
+    
+    return answer;
+}
