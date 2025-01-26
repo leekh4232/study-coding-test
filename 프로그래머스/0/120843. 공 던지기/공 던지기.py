79b5a2adfb494ae7742a7e450f52e2ffc2979747
이광호 <leekh4232@gmail.com>
2025-01-27 01:20:10 +0900

    [level 0] Title: 공 던지기, Time: 0.11 ms, Memory: 9.76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -0,0 +1,13 @@
+def solution(numbers, k):
+    answer = 0
+    p = 0
+
+    for i in range(k):
+        answer = numbers[p]
+        p = (p + 2) % len(numbers)
+
+    return answer
+
+if __name__ == "__main__":
+    print(solution([1,2,3,4], 2))
+    print(solution([1,2,3,4,5,6], 5))
