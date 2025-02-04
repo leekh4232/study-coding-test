39c7a3e2aa1a97ce21c78caf09e42f42aa5dc42a
이광호 <leekh4232@gmail.com>
2025-02-04 21:40:57 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,13 @@
+def solution(s):
+    s = s.split()
+    
+    stack=[]
+    
+    for x in s:
+        if x=='Z':
+            stack.pop()
+        else:
+            stack.append(int(x))
+
+    
+    return sum(stack)
