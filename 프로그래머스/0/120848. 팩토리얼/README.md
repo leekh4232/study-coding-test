3bcbed277b472f6029239c2d82b7c42d41e0d939
이광호 <leekh4232@gmail.com>
2025-02-10 02:13:33 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/README.md
+++ 프로그래머스/0/120848. 팩토리얼/README.md
@@ -1,10 +1,10 @@
 # [level 0] 팩토리얼 - 120848 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120848) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120848?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 22일 21:16:09
+2025년 02월 10일 02:13:32
 
 ### 문제 설명
 
--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,20 +1,19 @@
 def solution(n):
-    prev_fact = 0
-    prev_i = 0
+    answer = 0
 
-    for i in range(n+1):
-        fact = 1
-        for j in range(2, i+1):
+    # 1부터 n까지 반복 (문제 제한 사항에서 n의 최소값은 1임)
+    for i in range(1, n+1):
+        fact = 1                # 구하고자 하는 팩토리얼 값
+        for j in range(2, i+1): # 2부터 i까지 누적곱을 구함 -> 팩토리얼
             fact *= j
 
-        if fact <= n:
-            prev_fact = fact
-            prev_i = i
-        else:
-            break
+        if fact <= n:           # 팩토리얼이 n보다 작거나 같다면?
+            answer = i          # answer에 i를 저장함
+        else:                   # 그렇지 않다면? -> 팩토리얼이 n을 초과한다면?
+            break               # 반복중단
 
-    return prev_i
+    # 리턴값은 n을 초과하기 직전의 팩토리얼에 대한 i값이므로 주어진 범위 안에서 가장 큰 값임.
+    return answer
 
-if __name__ == '__main__':
-    print(solution(3628800))
-    print(solution(7))
+print(solution(3628800))
+print(solution(7))
