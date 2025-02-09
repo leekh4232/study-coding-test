0f2b3488a2c9b461e4838b6dc716c26235a20569
이광호 <leekh4232@gmail.com>
2025-02-10 02:04:10 +0900

    [level 0] Title: 제곱수 판별하기, Time: 37.29 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/README.md
+++ 프로그래머스/0/120909. 제곱수 판별하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 제곱수 판별하기 - 120909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120909?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 34.92 ms
+메모리: 10.1 MB, 시간: 37.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 22일 17:27:30
+2025년 02월 10일 02:04:07
 
 ### 문제 설명
 
--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,9 +1,13 @@
 def solution(n):
-    answer = 2
+    answer = 2                  # 일단 제곱수가 아니라고 가정
     
+    # i를 제곱해서 n이 되기 위해서는 n의 1/2보다 클 수 없다.
     for i in range(2, n//2):
-        if i ** 2 == n:
-            answer = 1
+        if i ** 2 == n:         # i의 제곱이 n이라면?
+            answer = 1          # 제곱수가 맞다는 의미의 값으로 변경
             break
     
     return answer
+
+print(solution(144))    # 1
+print(solution(979))    # 2
