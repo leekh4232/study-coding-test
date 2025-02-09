8d9fb128b344038e215b2e2733d071f04e381533
이광호 <leekh4232@gmail.com>
2025-02-10 02:23:40 +0900

    [level 0] Title: 합성수 찾기, Time: 0.19 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/README.md
+++ 프로그래머스/0/120846. 합성수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 합성수 찾기 - 120846 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120846) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120846?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.10 ms
+메모리: 10.1 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 22일 21:08:04
+2025년 02월 10일 02:23:38
 
 ### 문제 설명
 
--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,16 +1,18 @@
 def solution(n):
-    answer = 0
+    answer = 0                      # 합성수의 개수
 
-    for i in range(1, n + 1):
-        count = 0
+    for i in range(1, n + 1):       # n이하의 자연수 이므로 1부터 n까지 반복
+        count = 0                   # i의 약수에 대한 카운트 변수
+
+        # 1부터 i까지 반복하면서 약수의 개수를 카운트한다.
         for j in range(1, i+1):
-            if i % j == 0:
-                count += 1
-                if (count >= 3):
-                    answer += 1
-                    break
+            if i % j == 0:          # j가 i의 약수라면?
+                count += 1          # 카운트 1 증가
+                if count >= 3:      # 약수의 개수가 3개 이상이라면?
+                    answer += 1     # 합성수의 개수 1증가
+                    break           # 3개를 초과하는 약수의 수는 의미 없으므로 반복 중단
 
     return answer
 
-if __name__ == '__main__':
-    print(solution(10))
+print(solution(10))     # 5
+print(solution(15))     # 8
