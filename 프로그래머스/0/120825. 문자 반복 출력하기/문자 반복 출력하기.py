974af861e283eb6b03c0b3400e945982bea75140
이광호 <leekh4232@gmail.com>
2025-02-12 05:16:58 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,9 +1,9 @@
 def solution(my_string, n):
-    answer = ''
-    
-    for m in my_string:
-        answer += m * n
-    
-    return answer
+    answer = ''  # 결과를 저장할 빈 문자열 초기화
+
+    for m in my_string:  # my_string의 각 문자를 순회
+        answer += m * n  # 각 문자를 n번 반복하여 answer에 추가
+
+    return answer  # 최종 결과 반환
 
 print(solution("hello", 3))
