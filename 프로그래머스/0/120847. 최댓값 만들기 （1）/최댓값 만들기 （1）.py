64c223d7822d640cc372a0f20bb856b11077493f
이광호 <leekh4232@gmail.com>
2025-02-11 12:17:31 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.38 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,13 +1,16 @@
 def solution(numbers):
     answer = 0
-    
+
     n = len(numbers)
-        
+
     for i in range(0, n):
         for j in range(i+1, n):
             p = numbers[i] * numbers[j]
-            
+
             if p > answer:
                 answer = p
 
     return answer
+
+print(solution([1, 2, 3, 4, 5]))        # 20
+print(solution([0, 31, 24, 10, 1, 9]))  # 744
