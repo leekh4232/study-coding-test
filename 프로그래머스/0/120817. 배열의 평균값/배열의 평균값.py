a39fc7695e4b0492792c97a66771990c49e61649
이광호 <leekh4232@gmail.com>
2025-02-11 12:10:51 +0900

    [level 0] Title: 배열의 평균값, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,12 +1,7 @@
 def solution(numbers):
-    answer = 0
-    
-    size = len(numbers)
-    sum = 0
-    
-    for n in numbers:
-        sum += n
-    
-    answer = sum / size
-    
+    # 리스트 원소의 총합 / 리스트 원소의 개수
+    answer = sum(numbers) / len(numbers)
     return answer
+
+print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))    # 5.5
+print(solution([89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]))  # 94.0
