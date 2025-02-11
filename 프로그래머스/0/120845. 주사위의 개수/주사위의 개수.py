f94d25c6a332fe418c26e6407dc9e66c3fd2de3a
이광호 <leekh4232@gmail.com>
2025-02-11 12:11:34 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,10 +1,13 @@
 def solution(box, n):
     #answer = (box[0] // n) * (box[1] // n) * (box[2] // n)
     #return answer
-    
+
     answer = 1
-    
+
     for b in box:
         answer *= b // n
-        
+
     return answer
+
+print(solution([1, 1, 1], 1))    # 1
+print(solution([10, 8, 6], 3))   # 125
