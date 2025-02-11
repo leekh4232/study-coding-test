73a57ee909afbafb355b599411acd285405dba31
이광호 <leekh4232@gmail.com>
2025-02-12 05:28:55 +0900

    [level 0] Title: 암호 해독, Time: 0.21 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,7 +1,11 @@
 def solution(cipher, code):
     answer = ''
-    
+
+    # code의 배수 위치에 있는 문자를 answer에 추가
     for i in range(code, len(cipher)+1, code):
         answer += cipher[i-1]
-    
+
     return answer
+
+print(solution("dfjardstddetckdaccccdegk", 4))      # attack
+print(solution("pfqallllabwaoclk", 2))              # fallback
