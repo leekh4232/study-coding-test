be720942299cbf485c7492df55a045b73e2fb0fb
이광호 <leekh4232@gmail.com>
2025-02-12 05:33:14 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/README.md
+++ 프로그래머스/0/120826. 특정 문자 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특정 문자 제거하기 - 120826 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120826) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120826?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 22일 12:04:38
+2025년 02월 12일 05:33:12
 
 ### 문제 설명
 
--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,3 +1,15 @@
 def solution(my_string, letter):
-    answer = my_string.replace(letter, '')
+    # [풀이1] 문자열의 replace() 메서드 사용
+    #answer = my_string.replace(letter, '')
+
+    # [풀이2] 문자열을 순회하면서 letter와 같지 않은 문자만 answer에 추가
+    answer = ''
+
+    for m in my_string:
+        if m != letter:
+            answer += m
+
     return answer
+
+print(solution("abcdef", "f"))      # abcde
+print(solution("BCBdbe", "B"))      # Cdbe
