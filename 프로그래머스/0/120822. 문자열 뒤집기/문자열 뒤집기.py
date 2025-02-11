b9ccacaf13631315a1edd6eb2c997f27eaf8ee91
이광호 <leekh4232@gmail.com>
2025-02-12 05:19:52 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,9 +1,12 @@
 def solution(my_string):
-    answer = list(my_string)
-    n = len(answer)
-    
-    for i in range(n//2):
-        p = n - i - 1
-        answer[i], answer[p] = answer[p], answer[i]
-        
-    return "".join(answer)
+    answer = list(my_string)    # 문자열을 리스트로 변환하여 answer에 저장
+    n = len(answer)             # 문자열의 길이를 n에 저장
+
+    for i in range(n//2):       # 문자열의 절반 길이만큼 반복
+        p = n - i - 1           # 뒤에서부터의 인덱스를 계산
+        answer[i], answer[p] = answer[p], answer[i]  # 앞뒤 문자를 교환
+
+    return "".join(answer)      # 리스트를 다시 문자열로 변환하여 반환
+
+print(solution("jaron"))
+print(solution("bread"))
