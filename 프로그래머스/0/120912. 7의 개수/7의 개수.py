c6d9bd80316141188bfba19281dccd7dd433bef9
이광호 <leekh4232@gmail.com>
2025-02-12 04:42:36 +0900

    [level 0] Title: 7의 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,2 +1,27 @@
 def solution(array):
+    # [풀이1]
+    # 리스트의 요소를 하나씩 꺼내서 문자열로 변경하여 7인 경우 카운트
+    # answer = 0
+    #
+    # for i in array:
+    #     if str(i) == "7":
+    #         answer += 1
+    #
+    # return answer
+
+    # [풀이2]
+    # 리스트의 요소를 하나씩 꺼내서 문자열로 변경하여 하나의 문자열로 결합
+    # mystr = ""
+
+    # for i in array:
+    #     mystr += str(i)
+
+    # 문자열에서 7의 개수를 카운트하여 리턴
+    # return mystr.count("7")
+
+    # [풀이3]
+    # 파이썬의 str() 함수를 사용하여 리스트를 문자열로 변환후 카운트
     return str(array).count("7")
+
+print(solution([7, 77, 17]))    # 4
+print(solution([10, 29]))       # 0
--- 프로그래머스/0/120912. 7의 개수/README.md
+++ 프로그래머스/0/120912. 7의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 7의 개수 - 120912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120912) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120912?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 10.3 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 27일 01:42:01
+2025년 02월 12일 04:42:34
 
 ### 문제 설명
 
