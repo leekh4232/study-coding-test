5a94befdf876dc8d85b6b12a302e138e745e0f5b
이광호 <leekh4232@gmail.com>
2025-02-11 11:36:06 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -3,9 +3,12 @@ def solution(common):
     if common[1] - common[0] == common[2] - common[1]:
         # 등차수열
         answer = common[-1]+(common[1]-common[0])
-        
+
     elif common[1]/common[0] == common[2]/common[1]:
         # 등비수열
         answer = common[-1]*(common[1]/common[0])
-        
+
     return answer
+
+print(solution([1,2,3,4]))  # 5
+print(solution([2,4,8]))    # 16
