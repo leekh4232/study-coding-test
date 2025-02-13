b3f6ab8bf437713db2d806f983217851bc354aee
이광호 <leekh4232@gmail.com>
2025-02-13 21:32:48 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자연수 뒤집어 배열로 만들기 - 12932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.04 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 14일 16:18:06
+2025년 02월 13일 21:32:44
 
 ### 문제 설명
 
--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,28 +1,7 @@
-"""
-프로그래머스 12932번 - 자연수 뒤집어 배열로 만들기
-
-https://school.programmers.co.kr/learn/courses/30/lessons/12932
-"""
 def solution(n):
-    # [풀이1] 특정 위치의 원소와 교환할 반대쪽 위치를 계산하는 형태
-    # k = list(str(n))
-
-    # size = len(k)
-    # half = size // 2
-
-    # for i in range(half):
-    #     p = size - i - 1
-
-    #     #k[i], k[p] = k[p], k[i]
-    #     a = k[i]
-    #     k[i] = k[p]
-    #     k[p] = a
-
-    # return list(map(int, k))
-
-    # [풀이2] 문자열로 변환한 뒤, 문자열을 뒤집어서 리스트로 변환
-    return list(map(int, reversed(str(n))))
-
-if __name__ == '__main__':
-    n = 12345
-    print(solution(n))
+    result = []
+    while n > 0:
+        num = n % 10
+        n //= 10
+        result.append(num)
+    return result
