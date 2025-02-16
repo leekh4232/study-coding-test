947bbcc7737618febe1802e06b64207c02925374
이광호 <leekh4232@gmail.com>
2025-02-16 15:33:44 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/README.md
+++ 프로그래머스/0/120839. 가위 바위 보/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가위 바위 보 - 120839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 25일 15:44:09
+2025년 02월 16일 15:33:42
 
 ### 문제 설명
 
--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,9 +1,13 @@
 def solution(rsp):
-    answer = ''
+    # 변환 규칙을 딕셔너리로 정의
+    win_dict = {'0': '5', '2': '0', '5': '2'}
     
-    mydic = {'0':'5','2':'0','5':'2'}
+    # 리스트 컴프리헨션을 사용하여 변환된 값을 저장 (성능 최적화)
+    answer = [win_dict[r] for r in rsp]
     
-    for r in rsp:
-        answer += mydic[r]
-    
-    return answer
+    # 리스트를 문자열로 변환하여 반환
+    return ''.join(answer)
+
+# 테스트 실행
+print(solution("2"))     # 출력: "0"
+print(solution("205"))   # 출력: "052"
