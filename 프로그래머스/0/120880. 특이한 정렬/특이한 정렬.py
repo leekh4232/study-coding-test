b25b03d2102509caaeb04591678a3bcade7ecb08
이광호 <leekh4232@gmail.com>
2025-02-16 11:52:46 +0900

    [level 0] Title: 특이한 정렬, Time: 1.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -6,7 +6,8 @@ def solution(numlist, n):
         min_idx = i  # 현재 위치를 최소값 인덱스로 설정
 
         for j in range(i + 1, length):
-            if (abs(numlist[j] - n), -numlist[j]) < (abs(numlist[min_idx] - n), -numlist[min_idx]):
+            if (abs(numlist[j] - n), -numlist[j]) < (
+                abs(numlist[min_idx] - n), -numlist[min_idx]):
                 min_idx = j  # 최소값 인덱스 갱신
 
         numlist[i], numlist[min_idx] = numlist[min_idx], numlist[i]  # 위치 변경
@@ -14,5 +15,8 @@ def solution(numlist, n):
     return numlist
 
 # ✅ 예제 테스트 실행
-print(solution([1, 2, 3, 4, 5, 6], 4))  # [4, 5, 3, 6, 2, 1]
-print(solution([10000, 20, 36, 47, 40, 6, 10, 7000], 30))  # [36, 40, 20, 47, 10, 6, 7000, 10000]
+print(solution([1, 2, 3, 4, 5, 6], 4))
+# 결과 [4, 5, 3, 6, 2, 1]
+
+print(solution([10000, 20, 36, 47, 40, 6, 10, 7000], 30))
+# 결과 [36, 40, 20, 47, 10, 6, 7000, 10000]
