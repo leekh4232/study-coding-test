e8715e0633637078db4e271fe17914a81cb05204
이광호 <leekh4232@gmail.com>
2025-02-16 15:46:50 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/README.md
+++ 프로그래머스/0/120882. 등수 매기기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 등수 매기기 - 120882 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120882) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120882?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 31일 09:00:09
+2025년 02월 16일 15:46:48
 
 ### 문제 설명
 
--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,9 +1,23 @@
 def solution(score):
-    avg = [sum(i)/2 for i in score] # 평균
-    s_avg = sorted(avg, reverse=True) # 평균을 큰 값부터 내림차순 정렬
+    # 각 학생의 평균 점수를 계산
+    avg = []  # 평균 점수를 저장할 리스트
+    for i in score:
+        # 각 학생의 점수를 더한 후 2로 나누어 평균 계산
+        avg.append(sum(i) / 2)
 
-    answer =[] # 등수
+    # 평균 점수를 내림차순으로 정렬
+    s_avg = sorted(avg, reverse=True)
+
+    answer = []  # 등수를 저장할 리스트
     for i in avg:
-        answer.append(s_avg.index(i)+1) # index는 맨 앞에 위치한 요소의 인덱스 번호를 리턴
-    
-    return answer
+        # 정렬된 리스트에서 인덱스를 찾아 등수 부여
+        answer.append(s_avg.index(i) + 1)
+
+    return answer  # 등수 리스트 반환
+
+# 테스트 코드 실행
+print(solution([[80, 70], [90, 50], [40, 70], [50, 80]]))
+# 결과: [1, 2, 4, 3]
+
+print(solution([[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]]))
+# 결과: [4, 4, 6, 2, 2, 1, 7]
