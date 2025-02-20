97aabf05a1a5f3b4acb824e1011813d328495152
이광호 <leekh4232@gmail.com>
2025-02-20 21:25:02 +0900

    [level 2] Title: 주식가격, Time: 25.66 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,12 +1,22 @@
-def solution(prices):
-    n = len(prices)  # 주식 가격 배열의 길이
-    answer = [0] * n  # 결과 배열 초기화
+from collections import deque
 
-    for i in range(n):  # 각 시점별 가격 확인
-        for j in range(i + 1, n):  # 이후 가격과 비교
-            answer[i] += 1
-            if prices[i] > prices[j]:  # 가격이 떨어지면 중단
-                break
+def solution(prices):
+    n = len(prices)
+    answer = [0] * n  # 결과 배열 초기화
+    stack = deque()  # 가격이 떨어질 가능성이 있는 인덱스를 저장할 스택
+
+    for i, v in enumerate(prices):
+        # 스택이 비어있지 않고, 현재 가격이 스택 최상단 가격보다 낮다면
+        while stack and prices[stack[-1]] > v:
+            j = stack.pop()  # 스택에서 인덱스 꺼내기
+            answer[j] = i - j  # 가격이 유지된 기간 계산
+
+        stack.append(i)  # 현재 인덱스 추가
+
+    # 스택에 남아있는 인덱스들은 끝까지 가격이 떨어지지 않은 경우
+    while stack:
+        j = stack.pop()
+        answer[j] = n - 1 - j
 
     return answer
 
