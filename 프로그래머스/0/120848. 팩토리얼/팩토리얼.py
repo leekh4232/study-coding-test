6bf600177b232edaeede2999fda1be714a435162
이광호 <leekh4232@gmail.com>
2025-02-21 13:08:58 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,19 +1,21 @@
 def solution(n):
-    answer = 0
+    answer = 0  # 가장 큰 i 값을 저장할 변수 초기화
 
     # 1부터 n까지 반복 (문제 제한 사항에서 n의 최소값은 1임)
     for i in range(1, n+1):
-        fact = 1                # 구하고자 하는 팩토리얼 값
-        for j in range(2, i+1): # 2부터 i까지 누적곱을 구함 -> 팩토리얼
-            fact *= j
+        fact = 1  # 구하고자 하는 팩토리얼 값 초기화
+        
+        for j in range(2, i+1):  # 2부터 i까지 반복하여 누적곱을 구함 (팩토리얼 계산)
+            fact *= j  # fact에 j를 곱하여 팩토리얼 값을 계산
 
-        if fact <= n:           # 팩토리얼이 n보다 작거나 같다면?
-            answer = i          # answer에 i를 저장함
-        else:                   # 그렇지 않다면? -> 팩토리얼이 n을 초과한다면?
-            break               # 반복중단
+        if fact <= n:  # 계산된 팩토리얼이 n보다 작거나 같다면?
+            answer = i  # 현재 i가 조건을 만족하므로 answer를 갱신
+        else:  # 만약 fact가 n을 초과하면 더 이상 검사할 필요 없음
+            break  # 반복 종료
 
-    # 리턴값은 n을 초과하기 직전의 팩토리얼에 대한 i값이므로 주어진 범위 안에서 가장 큰 값임.
+    # 가장 큰 i 값 반환
     return answer
 
-print(solution(3628800))
-print(solution(7))
+# 테스트 예제 실행
+print(solution(3628800))  # 10
+print(solution(7))        # 3
