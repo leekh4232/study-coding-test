4f241e99e1a81be5ece262d8fa4419c7d32ac29a
이광호 <leekh4232@gmail.com>
2025-02-21 14:51:12 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,15 +1,14 @@
 def solution(num_list):
-    # 풀이 [1] - 파이썬 기능 사용
-    #num_list.reverse()
-    #return num_list
-    
-    # 풀이 [2] - 반대쪽 원소의 위치 구하기
+    # 리스트 길이 저장
     n = len(num_list)
-    for i in range(n//2):
-        p = n - i - 1
-        num_list[i], num_list[p] = num_list[p], num_list[i]
-        
-    return num_list
 
+    # 리스트의 절반만 순회하여 양 끝 요소를 교환
+    for i in range(n // 2):  
+        p = n - i - 1  # 반대쪽 요소의 인덱스
+        num_list[i], num_list[p] = num_list[p], num_list[i]  # 값 교환
+
+    return num_list  # 뒤집힌 리스트 반환
+
+# 테스트 예제 실행
 print(solution([1, 2, 3, 4, 5]))        # [5, 4, 3, 2, 1]
 print(solution([1, 1, 1, 1, 2]))        # [2, 1, 1, 1, 1]
