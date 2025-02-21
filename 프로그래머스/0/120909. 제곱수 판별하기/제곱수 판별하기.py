6eff4f1c82fddc21ee5c12c1d043fee0cbd03773
이광호 <leekh4232@gmail.com>
2025-02-21 13:06:41 +0900

    [level 0] Title: 제곱수 판별하기, Time: 35.76 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,13 +1,14 @@
 def solution(n):
-    answer = 2                  # 일단 제곱수가 아니라고 가정
-    
-    # i를 제곱해서 n이 되기 위해서는 n의 1/2보다 클 수 없다.
-    for i in range(2, n//2):
-        if i ** 2 == n:         # i의 제곱이 n이라면?
-            answer = 1          # 제곱수가 맞다는 의미의 값으로 변경
-            break
-    
-    return answer
+    answer = 2  # 기본적으로 n이 제곱수가 아니라고 가정
 
-print(solution(144))    # 1
-print(solution(979))    # 2
+    # i를 제곱해서 n이 되기 위해서는 n의 절반보다 클 필요가 없음.
+    for i in range(2, n//2):  # 2부터 n//2까지 반복 (1은 항상 제곱수의 기준이 될 수 있음)
+        if i ** 2 == n:  # i를 제곱했을 때 n과 같다면?
+            answer = 1  # n이 제곱수이므로 answer 값을 1로 변경
+            break  # 정답을 찾았으므로 더 이상 반복할 필요 없음
+
+    return answer  # 결과 반환
+
+# 테스트 예제 실행
+print(solution(144))  # 1 (12*12)
+print(solution(979))  # 2 (제곱수가 아님)
