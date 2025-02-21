9a62a780472db460ee8483ce90dbc5273b611bea
이광호 <leekh4232@gmail.com>
2025-02-21 12:57:15 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/README.md
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 구슬을 나누는 경우의 수 - 120840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 22일 21:18:56
+2025년 02월 21일 12:57:14
 
 ### 문제 설명
 
--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,12 +1,16 @@
 def fecto(n):
-    f = 1
-
-    for i in range(1, n+1):
-        f *= i
-
-    return f
+    f = 1  # 팩토리얼 값을 저장할 변수, 초기값은 1
+    
+    for i in range(1, n+1):  # 1부터 n까지 반복하여 곱함
+        f *= i  # 현재 숫자를 누적 곱셈
+    
+    return f  # 계산된 팩토리얼 값 반환
 
 def solution(balls, share):
-    n = balls
-    k = share
-    return fecto(n) // (fecto(k) * fecto(n-k))
+    n = balls  # 전체 구슬 개수
+    k = share  # 선택할 구슬 개수
+    return fecto(n) // (fecto(k) * fecto(n-k))  # 조합 공식 nCr = n! / (r! * (n-r)!) 적용하여 반환
+
+# 테스트 예제 실행
+print(solution(3, 2))  # 3
+print(solution(5, 3))  # 10
