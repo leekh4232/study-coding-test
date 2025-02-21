d72dea477ff19ab9b32b13c4c069f6688c24b06c
이광호 <leekh4232@gmail.com>
2025-02-21 14:59:28 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,18 +1,14 @@
 def solution(angle):
-    answer = 0
-
-    if angle < 90:
-        answer = 1
-    elif angle == 90:
-        answer = 2
+    #if angle > 0 and angle < 90:
+    if 0 < angle < 90:      # 예각
+        return 1
+    elif angle == 90:       # 직각
+        return 2
     #elif angle > 90 and angle < 180:
-    elif 90 < angle < 180:
-        answer = 3
-    #elif angle == 180:
-    else:
-        answer = 4
-
-    return answer
+    elif 90 < angle < 180:  # 둔각
+        return 3
+    elif angle == 180:      # 평각
+        return 4
 
 # ✅ 예제 테스트 실행
 print(solution(70))   # 결과: 1
