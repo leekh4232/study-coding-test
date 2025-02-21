4ff63e8875e869c7457b2c1744068a8ae1f26392
이광호 <leekh4232@gmail.com>
2025-02-21 16:23:16 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,15 +1,14 @@
 def fecto(n):
     f = 1  # 팩토리얼 값을 저장할 변수, 초기값은 1
-    
+
     for i in range(1, n+1):  # 1부터 n까지 반복하여 곱함
         f *= i  # 현재 숫자를 누적 곱셈
-    
+
     return f  # 계산된 팩토리얼 값 반환
 
 def solution(balls, share):
-    n = balls  # 전체 구슬 개수
-    k = share  # 선택할 구슬 개수
-    return fecto(n) // (fecto(k) * fecto(n-k))  # 조합 공식 nCr = n! / (r! * (n-r)!) 적용하여 반환
+    answer = fecto(balls) / (fecto(balls - share) * fecto(share))
+    return answer
 
 # 테스트 예제 실행
 print(solution(3, 2))  # 3
