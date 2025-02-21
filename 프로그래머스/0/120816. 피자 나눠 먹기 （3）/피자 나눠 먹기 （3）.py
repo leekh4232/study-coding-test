53de4d39d32c80d041f3fba24838f839a35bbb14
이광호 <leekh4232@gmail.com>
2025-02-21 16:06:08 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,12 +1,11 @@
 def solution(slice, n):
-    answer = 0
-    
-    if n % slice == 0:
-        answer = n // slice
-    else:
-        answer = n // slice + 1
-    
-    return answer
+    answer = n // slice  # 기본적으로 필요한 피자 판 수
 
-print(solution(7, 10))      # 2
-print(solution(4, 12))      # 3
+    if n % slice != 0:  # n이 slice로 나누어 떨어지지 않는 경우
+        answer += 1  # 추가로 한 판을 더 주문해야 모든 사람이 먹을 수 있음
+
+    return answer  # 최종적으로 필요한 피자 판 수 반환
+
+# 테스트 예제 실행
+print(solution(7, 10))  # 2
+print(solution(4, 12))  # 3
