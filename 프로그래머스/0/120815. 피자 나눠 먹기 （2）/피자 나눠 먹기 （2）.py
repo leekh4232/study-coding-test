c8cc97aadabb73330a669503d0c62ee287b39329
이광호 <leekh4232@gmail.com>
2025-02-21 13:10:53 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,18 +1,19 @@
 def solution(n):
-    # 필요한 피자의 수 -> 최소 한 판 부터 시작
+    # 필요한 피자의 최소 판 수 -> 최소 한 판부터 시작
     answer = 1
-    
-    # 최대 몇 판이 필요한지 모르는 상황이므로 무한루프를 수행한다.
+
+    # 최대 몇 판이 필요한지 모르는 상황이므로 무한 루프 수행
     while True:
-        # 피자의 판 수에 조각수를 곱한 값이 모든 사람이 나누어 먹을 수 있다면 반복 중단
+        # 피자의 판 수(answer)에 6조각을 곱한 값이 n으로 나누어 떨어지면 반복 중단
         if answer * 6 % n == 0:
-            break
+            break  # 최소한의 피자 판 수를 찾았으므로 루프 종료
 
-        # 반복을 중단하지 못하면 피자의 판 수를 1늘려서 반복을 계속한다.
+        # 조건을 만족하지 못하면 피자의 판 수를 1 증가
         answer += 1
-    
-    return answer
 
-print(solution(6))      # 1
-print(solution(10))     # 5
-print(solution(4))      # 2
+    return answer  # 계산된 최소 피자 판 수 반환
+
+# 테스트 예제 실행
+print(solution(6))  # 1
+print(solution(10)) # 5
+print(solution(4))  # 2
