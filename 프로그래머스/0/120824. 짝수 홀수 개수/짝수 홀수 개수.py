b5ddce1e44a3f535a1baf3a1464da789cab07303
이광호 <leekh4232@gmail.com>
2025-02-25 20:34:10 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,12 +1,11 @@
 def solution(num_list):
-    answer = [0, 0]
+    answer = [0, 0]  # 짝수 개수와 홀수 개수를 저장할 리스트
 
-    for n in num_list:
-        # 어떤 수 n을 2로 나누면 짝수인 경우는 0, 홀수인 경우는 1이 된다.
-        # answer에서 0번째 인덱스는 짝수의 개수, 1번째 인덱스는 홀수의 개수를 나타낸다.
-        answer[n % 2] += 1
+    for n in num_list:  # 리스트의 각 원소를 순회
+        answer[n % 2] += 1  # 짝수면 answer[0] 증가, 홀수면 answer[1] 증가
 
-    return answer
+    return answer  # [짝수 개수, 홀수 개수] 반환
 
+# 테스트 예제 실행
 print(solution([1, 2, 3, 4, 5]))    # [2, 3]
 print(solution([1, 3, 5, 7]))       # [0, 4]
