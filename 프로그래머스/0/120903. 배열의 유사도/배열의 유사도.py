c1fe112e2cc812b014d540cfbe1505ddb433d58a
이광호 <leekh4232@gmail.com>
2025-02-25 19:40:57 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,14 +1,12 @@
 def solution(s1, s2):
-    # 동일한 원소를 카운트할 변수
-    answer = 0
+    answer = 0  # 공통 원소 개수를 저장할 변수
 
-    # 첫 번째 리스트의 원소에 대해 반복
-    for i in s1:
-        # 두 번째 리스트에 동일한 원소가 있다면 카운트 1 증가
-        if i in s2:
-            answer += 1
+    for i in s1:  # 첫 번째 리스트의 원소를 순회
+        if i in s2:  # 두 번째 리스트에 동일한 원소가 존재하면
+            answer += 1  # 공통 원소 개수 증가
 
-    return answer
+    return answer  # 공통 원소 개수 반환
 
+# 테스트 예제 실행
 print(solution(["a", "b", "c"], ["com", "b", "d", "p", "c"]))   # 2
 print(solution(["n", "omg"], ["m", "dot"]))                     # 0
