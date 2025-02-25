16cdaf9ebffe491bc7db1d0e33699da5ff5ca732
이광호 <leekh4232@gmail.com>
2025-02-25 21:28:02 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,23 +1,19 @@
 def solution(my_string):
-    # 문자열을 공백 기준으로 잘라서 리스트로 변환 --> ["3", "+", "4"]
-    my_list = my_string.split()
+    # 문자열을 공백 기준으로 분리하여 리스트로 변환
+    my_list = my_string.split()  
 
-    # 리스트의 첫 번째 요소를 정수로 변환하여 answer에 저장
+    # 첫 번째 숫자를 정수로 변환하여 초기값으로 설정
     answer = int(my_list[0])
 
-    # 리스트의 두 번째 요소부터 마지막 요소까지 반복
-    # 연산자, 숫자, 연산자, 숫자 ... 의 순서이므로 1부터 시작하여 2씩 증가
-    for i in range(1, len(my_list)-1, 2):
-        # 연산자가 "+"인 경우에는 다음 숫자를 더하고
-        if my_list[i] == '+':
+    # 1부터 2씩 증가하면서 연산자와 숫자를 순차적으로 처리
+    for i in range(1, len(my_list) - 1, 2):
+        if my_list[i] == '+':  # "+" 연산자이면 다음 숫자를 더함
             answer += int(my_list[i+1])
-        # 연산자가 "-"인 경우에는 다음 숫자를 뺀다.
-        else:
+        else:  # "-" 연산자이면 다음 숫자를 뺌
             answer -= int(my_list[i+1])
 
-    return answer
+    return answer  # 계산된 최종 결과 반환
 
+# 테스트 예제 실행
 print(solution("3 + 4"))    # 7
-
-# 프로그래머스에서 제시하지 않는 또 다른 테스트 케이스
 print(solution("5 + 7 - 2 - 5 + 10")) # 15
