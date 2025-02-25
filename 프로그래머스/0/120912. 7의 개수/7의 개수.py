80907789fb4f59d7f23950192586423288ed49ab
이광호 <leekh4232@gmail.com>
2025-02-25 20:53:26 +0900

    [level 0] Title: 7의 개수, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,27 +1,6 @@
 def solution(array):
-    # [풀이1]
-    # 리스트의 요소를 하나씩 꺼내서 문자열로 변경하여 7인 경우 카운트
-    # answer = 0
-    #
-    # for i in array:
-    #     if str(i) == "7":
-    #         answer += 1
-    #
-    # return answer
+    return str(array).count("7")  # 리스트 전체를 문자열로 변환 후 "7"의 개수 카운트
 
-    # [풀이2]
-    # 리스트의 요소를 하나씩 꺼내서 문자열로 변경하여 하나의 문자열로 결합
-    # mystr = ""
-
-    # for i in array:
-    #     mystr += str(i)
-
-    # 문자열에서 7의 개수를 카운트하여 리턴
-    # return mystr.count("7")
-
-    # [풀이3]
-    # 파이썬의 str() 함수를 사용하여 리스트를 문자열로 변환후 카운트
-    return str(array).count("7")
-
+# 테스트 예제 실행
 print(solution([7, 77, 17]))    # 4
 print(solution([10, 29]))       # 0
