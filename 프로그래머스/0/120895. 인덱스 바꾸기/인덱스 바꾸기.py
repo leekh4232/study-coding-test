db882f110050770babdaed979327d9aea8b8f0f1
이광호 <leekh4232@gmail.com>
2025-02-27 13:47:14 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,16 +1,11 @@
 def solution(my_string, num1, num2):
-    answer = ''
-
-    # 문자열을 리스트로 변환 -> 한 글자씩 리스트의 원소가 된다.
-    my_list = list(my_string)
+    my_list = list(my_string)  # 문자열을 리스트로 변환
 
     # num1과 num2의 위치에 있는 문자를 교환
     my_list[num1], my_list[num2] = my_list[num2], my_list[num1]
 
-    # 리스트를 문자열로 변환
-    answer = "".join(my_list)
+    return "".join(my_list)  # 리스트를 다시 문자열로 변환하여 반환
 
-    return answer
-
-print(solution("hello", 1, 2))          # hlelo
-print(solution("I love you", 3, 6))     # I l veoyou
+# 테스트 예제 실행
+print(solution("hello", 1, 2))          # "hlelo"
+print(solution("I love you", 3, 6))     # "I l veoyou"
