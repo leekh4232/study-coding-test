a29335d7e43c7ff8b1c8d4714b81583ea6c4081b
이광호 <leekh4232@gmail.com>
2025-02-27 20:38:44 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,39 +1,28 @@
-"""
-프로그래머스 42586번 - 기능개발
-https://school.programmers.co.kr/learn/courses/30/lessons/42586
-"""
-import math
 from collections import deque
 
 def solution(progresses, speeds):
+    progresses = deque(progresses)  # 작업 진행도를 큐로 변환
+    speeds = deque(speeds)  # 작업 속도를 큐로 변환
+
     answer = []
-    days = deque()
+    days = 0  # 경과 일수
+    cnt = 0  # 배포될 기능 개수
 
-    for i in range(len(progresses)):
-        d = math.ceil((100 - progresses[i]) / speeds[i])
-        days.append(d)
-
-    cnt = 1  # 완료된 기능
-    prev = days.popleft()
-
-    while len(days) > 0:
-        cur = days.popleft()
-        if prev >= cur:
-            cnt += 1
+    while progresses:  # 큐가 빌 때까지 반복
+        # 첫 번째 작업이 완료되었는지 확인 (소수점 올림)
+        if (progresses[0] + days * speeds[0]) >= 100:
+            progresses.popleft()  # 완료된 작업 제거
+            speeds.popleft()  # 해당 작업의 속도도 제거
+            cnt += 1  # 배포될 기능 개수 증가
         else:
-            answer.append(cnt)
-            cnt = 1
-            prev = cur
+            if cnt > 0:  # 배포할 기능이 있는 경우
+                answer.append(cnt)  # 배포 개수 저장
+                cnt = 0  # 배포 개수 초기화
+            days += 1  # 하루 증가
 
-    answer.append(cnt)
+    answer.append(cnt)  # 마지막 배포 개수 추가
     return answer
 
-
-if __name__ == "__main__":
-    progresses = [93, 30, 55]
-    speeds = [1, 30, 5]
-    print(solution(progresses, speeds))  # [2, 1]
-
-    progresses = [95, 90, 99, 99, 80, 99]
-    speeds = [1, 1, 1, 1, 1, 1]
-    print(solution(progresses, speeds))  # [1, 3, 2]
+# 테스트 예제 실행
+print(solution([93, 30, 55], [1, 30, 5]))  # [2, 1]
+print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))  # [1, 3, 2]
