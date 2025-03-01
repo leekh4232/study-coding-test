ba13fdba87578bfeeb824e69b17cc77d08ebd383
이광호 <leekh4232@gmail.com>
2025-03-01 10:39:48 +0900

    [level 0] Title: 두 수의 합, Time: 0.02 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
@@ -1,6 +1,12 @@
-class Solution {
+public class Solution {
     public int solution(int num1, int num2) {
         int answer = num1 + num2;
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(2, 3));
+        System.out.println(s.solution(100, 2));
     }
 }
