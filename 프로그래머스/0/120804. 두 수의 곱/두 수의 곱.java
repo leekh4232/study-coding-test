21ed8ee5f9b0d02ef990824415cf155e3a1ecb8b
이광호 <leekh4232@gmail.com>
2025-03-01 11:23:33 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -1,11 +1,5 @@
-public class Solution {
+class Solution {
     public int solution(int num1, int num2) {
         return num1 * num2;
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.solution(3, 4));
-        System.out.println(s.solution(27, 19));
     }
 }
