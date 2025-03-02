09d088e952cf5d6efb1429b21de7910449241f5e
이광호 <leekh4232@gmail.com>
2025-03-02 12:23:59 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/README.md
+++ 프로그래머스/0/120807. 숫자 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숫자 비교하기 - 120807 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120807?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120807?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 88.8 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 21일 12:38:00
+2025년 03월 02일 12:23:57
 
 ### 문제 설명
 
--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,5 +1,20 @@
-class Solution {
+public class Solution {
     public int solution(int num1, int num2) {
-        return num1 == num2 ? 1 : -1;
+        // 일단 두 수가 같다고 가정
+        int answer = 1;
+
+        // 두 수가 다르면 -1로 변경
+        if (num1 != num2) {
+            answer = -1;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(2, 3));
+        System.out.println(s.solution(11, 11));
+        System.out.println(s.solution(7, 9));
     }
 }
