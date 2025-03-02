193e9f0a146b99f6538b17408c47dedebb5e97ed
이광호 <leekh4232@gmail.com>
2025-03-02 12:29:31 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.04 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/README.md
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 피자 나눠 먹기 (3) - 120816 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120816?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120816?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 85.8 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 21일 16:06:27
+2025년 03월 02일 12:29:28
 
 ### 문제 설명
 
--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,13 +1,21 @@
-class Solution {
+public class Solution {
     public int solution(int slice, int n) {
-        int answer = 0;
-        
-        if (n % slice == 0) {
-            answer = n / slice;
-        } else {
-            answer = n / slice + 1;
+        // 기본적으로 필요한 피자 판 수
+        int answer = n / slice;
+
+        // n이 slice로 나누어 떨어지지 않는 경우
+        if (n % slice != 0) {
+            // 추가로 한 판을 더 주문해야 모든 사람이 먹을 수 있음
+            answer++;
         }
-        
+
+        // 최종적으로 필요한 피자 판 수 반환
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(7, 10));
+        System.out.println(s.solution(4, 12));
     }
 }
