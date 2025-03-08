d36db389fcdb113994073c5b29b7c2809f1e728a
이광호 <leekh4232@gmail.com>
2025-03-08 15:28:13 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 472.60 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870?language=python3) 
 
 ### 성능 요약
 
-메모리: 51.4 MB, 시간: 414.70 ms
+메모리: 18.7 MB, 시간: 472.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 12일 04:58:07
+2025년 03월 08일 15:28:10
 
 ### 문제 설명
 
--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,41 +1,32 @@
-"""
-프로그래머스 178870번 - 연속된 부분 수열의 합
-
-https://school.programmers.co.kr/learn/courses/30/lessons/178870
-"""
-
 def solution(sequence, k):
     # 투 포인터 초기화
-    left, right = 0, 0
-    current_sum = sequence[0]
-    min_length = float('inf')
-    answer = []
+    left, right = 0, 0  # 두 개의 포인터를 0으로 초기화
+    current_sum = sequence[0]  # 현재 부분합을 첫 번째 원소로 초기화
+    min_length = float('inf')  # 최소 길이를 무한대로 설정
+    answer = []  # 결과 저장 리스트
 
-    while right < len(sequence):
-        if current_sum == k:
-            # 현재 구간의 길이 계산
-            current_length = right - left
-            # 최소 길이 갱신
-            if current_length < min_length:
-                min_length = current_length
-                answer = [left, right]
-            # 왼쪽 포인터 이동
-            current_sum -= sequence[left]
-            left += 1
-        elif current_sum < k:
-            # 오른쪽 포인터 이동
-            right += 1
-            if right < len(sequence):
-                current_sum += sequence[right]
-        else:
-            # 왼쪽 포인터 이동
-            current_sum -= sequence[left]
-            left += 1
+    while right < len(sequence):  # 오른쪽 포인터가 수열 범위를 넘지 않도록 설정
+        if current_sum == k:  # 부분합이 k와 같을 경우
+            current_length = right - left  # 현재 부분 수열의 길이 계산
+            if current_length < min_length:  # 최소 길이 갱신 조건 확인
+                min_length = current_length  # 최소 길이 갱신
+                answer = [left, right]  # 현재 인덱스 저장
+            current_sum -= sequence[left]  # 왼쪽 포인터 이동을 위해 값 제거
+            left += 1  # 왼쪽 포인터 증가
 
-    return answer
+        elif current_sum < k:  # 부분합이 k보다 작다면
+            right += 1  # 오른쪽 포인터 이동
+            if right < len(sequence):  # 범위를 벗어나지 않도록 확인
+                current_sum += sequence[right]  # 새로운 값 추가
+
+        else:  # 부분합이 k보다 크다면
+            current_sum -= sequence[left]  # 왼쪽 포인터 이동을 위해 값 제거
+            left += 1  # 왼쪽 포인터 증가
+
+    return answer  # 찾은 부분 수열의 시작 인덱스와 끝 인덱스 반환
 
 if __name__ == "__main__":
     # 테스트 케이스 출력
-    print(solution([1, 2, 3, 4, 5], 7)) # [2, 3]
-    print(solution([1, 1, 1, 2, 3, 4, 5], 5)) # [6, 6]
-    print(solution([2, 2, 2, 2, 2], 6)) # [0, 2]
+    print(solution([1, 2, 3, 4, 5], 7))  # [2, 3]
+    print(solution([1, 1, 1, 2, 3, 4, 5], 5))  # [6, 6]
+    print(solution([2, 2, 2, 2, 2], 6))  # [0, 2]
