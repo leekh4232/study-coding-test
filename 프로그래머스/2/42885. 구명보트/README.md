72f1f27dea4c8491dcec496b73e276d51aa5de8e
이광호 <leekh4232@gmail.com>
2025-03-08 15:32:09 +0900

    [level 2] Title: 구명보트, Time: 9.59 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=python3) 
 
 ### 성능 요약
 
-메모리: 57.5 MB, 시간: 10.23 ms
+메모리: 10.6 MB, 시간: 9.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 12일 04:02:10
+2025년 03월 08일 15:32:07
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,14 +1,14 @@
 def solution(people, limit):
     # 구명보트의 수를 저장할 변수 초기화
     answer = 0
-    
+
     # 사람들의 몸무게를 오름차순으로 정렬
     people.sort()
     # 가장 가벼운 사람의 인덱스
     i = 0
     # 가장 무거운 사람의 인덱스
     j = len(people) - 1
-    
+
     # 두 인덱스가 만나거나 교차할 때까지 반복
     while i <= j:
         # 구명보트 하나 추가
@@ -22,6 +22,10 @@ def solution(people, limit):
         else:
             # 가장 무거운 사람만 태우고 인덱스 감소
             j -= 1
-    
+
     # 필요한 구명보트의 수 반환
     return answer
+
+if __name__ == "__main__":
+    print(solution([70, 50, 80, 50], 100))  #3
+    print(solution([70, 80, 50], 100))      #3
