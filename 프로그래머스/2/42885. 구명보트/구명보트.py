e994b8df3c800cce8d2991aeb4d0c61e600b02fa
이광호 <leekh4232@gmail.com>
2025-03-10 17:03:11 +0900

    [level 2] Title: 구명보트, Time: 8.88 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -5,23 +5,23 @@ def solution(people, limit):
     # 사람들의 몸무게를 오름차순으로 정렬
     people.sort()
     # 가장 가벼운 사람의 인덱스
-    i = 0
+    S = 0
     # 가장 무거운 사람의 인덱스
-    j = len(people) - 1
+    E = len(people) - 1
 
     # 두 인덱스가 만나거나 교차할 때까지 반복
-    while i <= j:
+    while S <= E:
         # 구명보트 하나 추가
         answer += 1
         # 가장 가벼운 사람과 가장 무거운 사람의 몸무게 합이 제한 이하인 경우
-        if people[i] + people[j] <= limit:
+        if people[S] + people[E] <= limit:
             # 가장 가벼운 사람을 태우고 인덱스 증가
-            i += 1
+            S += 1
             # 가장 무거운 사람을 태우고 인덱스 감소
-            j -= 1
+            E -= 1
         else:
             # 가장 무거운 사람만 태우고 인덱스 감소
-            j -= 1
+            E -= 1
 
     # 필요한 구명보트의 수 반환
     return answer
