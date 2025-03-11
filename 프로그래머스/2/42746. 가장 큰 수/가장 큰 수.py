a3987aee3f41634a8febf745673db92ba347602e
이광호 <leekh4232@gmail.com>
2025-03-11 21:15:26 +0900

    [level 2] Title: 가장 큰 수, Time: 89.29 ms, Memory: 29.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,24 +1,13 @@
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    pivot = arr[len(arr) // 2]  # 피벗 설정 (가운데 요소 선택)
-    left = [x for x in arr if x + pivot > pivot + x]  # 피벗보다 앞에 와야 할 요소
-    middle = [x for x in arr if x + pivot == pivot + x]  # 동일한 경우
-    right = [x for x in arr if x + pivot < pivot + x]  # 피벗보다 뒤에 와야 할 요소
-
-    return quick_sort(left) + middle + quick_sort(right)
-
 def solution(numbers):
     # 숫자들을 문자열로 변환
     num_strs = list(map(str, numbers))
-    
-    # 커스텀 정렬 (퀵 정렬 적용)
-    sorted_numbers = quick_sort(num_strs)
-    
+
+    # 커스텀 정렬 (문자열 비교를 활용)
+    sorted_numbers = sorted(num_strs, key=lambda x: x * 10, reverse=True)
+
     # 정렬된 리스트를 이어붙여 최종 결과 생성
     result = "".join(sorted_numbers)
-    
+
     # 결과가 "000" 같은 경우 "0" 반환
     return result if result[0] != "0" else "0"
 
