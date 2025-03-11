daa6b2397317c11478cf8b9ac1c24a4e771e70b4
이광호 <leekh4232@gmail.com>
2025-03-11 14:34:23 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 14.74 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=java) 
 
 ### 성능 요약
 
-메모리: 18.7 MB, 시간: 97.60 ms
+메모리: 140 MB, 시간: 14.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 10일 17:05:10
+2025년 03월 11일 14:34:21
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,26 +1,30 @@
 import java.util.*;
 
-public class Solution {
+class Solution {
     public int solution(int[] queue1, int[] queue2) {
         // 두 큐의 합을 구함
-        long sum1 = Arrays.stream(queue1).asLongStream().sum();
-        long sum2 = Arrays.stream(queue2).asLongStream().sum();
+        long sum1 = Arrays.stream(queue1).sum();
+        long sum2 = Arrays.stream(queue2).sum();
 
         // 두 큐의 합이 같아야 하는 목표 값
         long target = (sum1 + sum2) / 2;
 
+        // 두 큐의 합이 홀수이면 나눌 수 없으므로 -1 반환
+        if ((sum1 + sum2) % 2 != 0) {
+            return -1;
+        }
+
         // 두 큐를 하나의 리스트로 합침
+        int n = queue1.length;
         int[] combined = new int[queue1.length + queue2.length];
         System.arraycopy(queue1, 0, combined, 0, queue1.length);
         System.arraycopy(queue2, 0, combined, queue1.length, queue2.length);
 
-        int n = queue1.length;
-
         // 투 포인터 초기화
-        int i = 0, j = n;
+        int S = 0, E = n; // S는 queue1의 시작, E는 queue2의 시작
 
         // 현재 합을 첫 번째 큐의 합으로 초기화
-        long currentSum = sum1;
+        long P = sum1;
 
         // 최대 이동 횟수는 두 큐의 길이의 합의 두 배
         int maxMoves = 2 * combined.length;
@@ -29,20 +33,20 @@ public class Solution {
         // 최대 이동 횟수 내에서 반복
         while (moves < maxMoves) {
             // 현재 합이 목표 값과 같으면 이동 횟수 반환
-            if (currentSum == target) {
+            if (P == target) {
                 return moves;
             }
             // 현재 합이 목표 값보다 작으면
-            else if (currentSum < target) {
+            else if (P < target) {
                 // 두 번째 큐에서 값을 가져와 현재 합에 더함
-                currentSum += combined[j];
+                P += combined[E];
                 // 두 번째 큐의 다음 인덱스로 이동
-                j = (j + 1) % combined.length;
+                E = (E + 1) % combined.length;
             } else {
                 // 현재 합이 목표 값보다 크면 첫 번째 큐에서 값을 빼줌
-                currentSum -= combined[i];
+                P -= combined[S];
                 // 첫 번째 큐의 다음 인덱스로 이동
-                i = (i + 1) % combined.length;
+                S = (S + 1) % combined.length;
             }
             // 이동 횟수 증가
             moves++;
@@ -54,8 +58,6 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-
-        // 테스트 케이스 출력
         System.out.println(sol.solution(new int[]{3, 2, 7, 2}, new int[]{4, 6, 5, 1})); // 2
         System.out.println(sol.solution(new int[]{1, 2, 1, 2}, new int[]{1, 10, 1, 2})); // 7
         System.out.println(sol.solution(new int[]{1, 1}, new int[]{1, 5})); // -1
