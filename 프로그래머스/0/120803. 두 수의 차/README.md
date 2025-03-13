8c904553a14878da9ae6efc102d44dea56e3258e
이광호 <leekh4232@gmail.com>
2025-03-14 08:08:37 +0900

    [level 0] Title: 두 수의 차, Time: 0.03 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/README.md
+++ 프로그래머스/0/120803. 두 수의 차/README.md
@@ -1,10 +1,10 @@
 # [level 0] 두 수의 차 - 120803 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120803?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120803?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 83.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 07일 11:22:16
+2025년 03월 14일 08:08:35
 
 ### 문제 설명
 
--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -1,5 +1,12 @@
-class Solution {
-    public int solution(int num1, int num2) {
-        return num1 - num2;
+public class Solution {
+    public static int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(solution(2, 3));     // 결과값: -1
+        System.out.println(solution(100, 2));   // 결과값: 98
     }
 }
+
