9ddf3f74a4576a19ae5ef4c8e4679d5d466bb486
이광호 <leekh4232@gmail.com>
2025-03-15 12:18:49 +0900

    [level 0] Title: 대문자와 소문자, Time: 16.13 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -3,14 +3,11 @@ class Solution {
         String result = "";  // 변환된 문자열을 저장할 변수
 
         // 문자열의 각 문자를 순회하며 변환
-        for (int i = 0; i < myString.length(); i++) {
-            String currentChar = myString.substring(i, i + 1);  // 현재 문자 가져오기
-            
-            // 대문자인 경우 소문자로 변환, 소문자인 경우 대문자로 변환
-            if (currentChar.equals(currentChar.toUpperCase())) {  
-                result += currentChar.toLowerCase();  // 대문자를 소문자로 변환
-            } else {
-                result += currentChar.toUpperCase();  // 소문자를 대문자로 변환
+        for (char c : myString.toCharArray()) {
+            if (Character.isUpperCase(c)) {  // 현재 문자가 대문자인 경우
+                result += Character.toLowerCase(c);  // 소문자로 변환하여 추가
+            } else {  // 현재 문자가 소문자인 경우
+                result += Character.toUpperCase(c);  // 대문자로 변환하여 추가
             }
         }
 
