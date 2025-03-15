db6520bee429fbb89b8664bca5f914ac74126174
이광호 <leekh4232@gmail.com>
2025-03-15 13:34:51 +0900

    [level 0] Title: 암호 해독, Time: 14.19 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,15 +1,13 @@
 class Solution {
     public String solution(String cipher, int code) {
-        // 결과를 저장할 StringBuilder 객체 초기화
-        StringBuilder answer = new StringBuilder();
+        String result = ""; // 결과를 저장할 문자열 초기화
 
         // code의 배수 번째 위치에 있는 문자만 추가
         for (int i = code - 1; i < cipher.length(); i += code) {
-            answer.append(cipher.charAt(i));
+            result += cipher.charAt(i); // 해당 위치의 문자 추가
         }
 
-        // 최종 해독된 문자열 반환
-        return answer.toString();
+        return result; // 최종 해독된 문자열 반환
     }
 
     public static void main(String[] args) {
