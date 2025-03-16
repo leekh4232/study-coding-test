e2f960f9668063eb5e6ae83b4f37c3300bf37f88
이광호 <leekh4232@gmail.com>
2025-03-16 16:20:01 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,13 +1,12 @@
-public class Solution {
+class Solution {
     public int solution(int chicken) {
-        // 받을 수 있는 총 서비스 치킨 수를 저장할 변수
-        int answer = 0;
+        int answer = 0; // 받을 수 있는 총 서비스 치킨 수
 
-        while (chicken >= 10) {         // 쿠폰이 10장 이상일 때만 교환 가능
-            int div = chicken / 10;     // 10개당 서비스 치킨 수 계산
-            int mod = chicken % 10;     // 교환 후 남은 쿠폰 수 계산
-            answer += div;              // 총 서비스 치킨 수에 추가
-            chicken = div + mod;        // 새로운 쿠폰 개수로 업데이트
+        while (chicken >= 10) { // 쿠폰이 10장 이상일 때만 교환 가능
+            int service = chicken / 10; // 10개당 서비스 치킨 수 계산
+            int remainder = chicken % 10; // 교환 후 남은 쿠폰 수 계산
+            answer += service; // 총 서비스 치킨 수에 추가
+            chicken = service + remainder; // 새로운 쿠폰 개수로 업데이트
         }
 
         return answer;
@@ -15,7 +14,9 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(100));
-        System.out.println(s.solution(1081));
+
+        // 테스트 예제 실행
+        System.out.println(s.solution(100));  // 11
+        System.out.println(s.solution(1081)); // 120
     }
 }
