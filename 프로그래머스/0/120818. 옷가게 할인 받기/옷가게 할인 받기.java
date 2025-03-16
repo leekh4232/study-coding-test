050a81ed946256a10d327958a0074bb88d029cf3
이광호 <leekh4232@gmail.com>
2025-03-16 16:16:57 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,23 +1,21 @@
-public class Solution {
+class Solution {
     public int solution(int price) {
-        int answer = 0;
-
         if (price >= 500000) {
-            answer = (int)(price * 0.8);
+            return (int) (price * 0.8); // 20% 할인 적용
         } else if (price >= 300000) {
-            answer = (int)(price * 0.9);
+            return (int) (price * 0.9); // 10% 할인 적용
         } else if (price >= 100000) {
-            answer = (int)(price * 0.95);
+            return (int) (price * 0.95); // 5% 할인 적용
         } else {
-            answer = price;
+            return price; // 할인 없음
         }
-
-        return answer;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(150000));
-        System.out.println(s.solution(580000));
+
+        // 테스트 예제 실행
+        System.out.println(s.solution(150000)); // 142500
+        System.out.println(s.solution(580000)); // 464000
     }
 }
