1e40fc0cc0cd759858d30e8b364ae1273f68085c
이광호 <leekh4232@gmail.com>
2025-03-16 12:37:23 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 16.48 ms, Memory: 7.63 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.py
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.py
@@ -1,9 +1,7 @@
-n = int(input())            # 정수 입력 (삼각형의 높이 및 너비 설정)
+n = int(input())
 
-for i in range(n):          # i(행) -> 0부터 n-1까지 반복
-    star = ""               # 매 행마다 출력할 별을 저장할 문자열을 초기화
-
-    for j in range(i+1):    # i번째 행에서 출력할 별의 개수 = i+1개
-        star += "*"         # 빈 문자열에 별을 하나씩 추가
-
-    print(star)             # 현재 행의 별 출력
+for i in range(n):
+    star = ""
+    for j in range(0, i+1):
+        star += "*"
+    print(star)
