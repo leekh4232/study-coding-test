730a741dc9f495d5b0ece3a6b578125d2d970b00
이광호 <leekh4232@gmail.com>
2025-03-16 16:18:53 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,21 +1,19 @@
-public class Solution {
+class Solution {
     public int solution(int slice, int n) {
-        // 기본적으로 필요한 피자 판 수
-        int answer = n / slice;
+        int answer = n / slice; // 기본적으로 필요한 피자 판 수 계산
 
-        // n이 slice로 나누어 떨어지지 않는 경우
-        if (n % slice != 0) {
-            // 추가로 한 판을 더 주문해야 모든 사람이 먹을 수 있음
+        if (n % slice != 0) { // 나머지가 있으면 한 판 추가
             answer++;
         }
 
-        // 최종적으로 필요한 피자 판 수 반환
-        return answer;
+        return answer; // 최종적으로 필요한 피자 판 수 반환
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(7, 10));
-        System.out.println(s.solution(4, 12));
+
+        // 테스트 예제 실행
+        System.out.println(s.solution(7, 10)); // 2
+        System.out.println(s.solution(4, 12)); // 3
     }
 }
