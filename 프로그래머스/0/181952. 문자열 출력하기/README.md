66b55df29672484dc2770256fe09102fab3129b8
이광호 <leekh4232@gmail.com>
2025-03-17 12:51:51 +0900

    [level 0] Title: 문자열 출력하기, Time: 172.86 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/README.md
+++ 프로그래머스/0/181952. 문자열 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 출력하기 - 181952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181952?language=go) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181952?language=java) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 3.60 ms
+메모리: 72.2 MB, 시간: 172.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 13일 16:35:23
+2025년 03월 17일 12:51:49
 
 ### 문제 설명
 
--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,9 +1,17 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        System.out.println(a);
+public class Solution { 
+    public static void main(String[] args) { 
+        // Scanner 객체 생성 (표준 입력을 받기 위함)
+        Scanner sc = new Scanner(System.in); 
+        
+        // 공백이 없는 단일 문자열을 입력받음
+        String a = sc.next(); 
+        
+        // 입력받은 문자열을 출력
+        System.out.println(a); 
+        
+        // Scanner 객체 닫기 (자원 해제)
+        sc.close(); 
     }
 }
