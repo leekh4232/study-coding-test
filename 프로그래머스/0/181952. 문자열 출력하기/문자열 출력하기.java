eec906248ba6576e74e4243dfe99fa403f068fc2
이광호 <leekh4232@gmail.com>
2025-03-17 15:24:34 +0900

    [level 0] Title: 문자열 출력하기, Time: 236.23 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,16 +1,16 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
-public class Solution { 
-    public static void main(String[] args) { 
+public class Solution {
+    public static void main(String[] args) {
         // Scanner 객체 생성 (표준 입력을 받기 위함)
-        Scanner sc = new Scanner(System.in); 
-        
+        Scanner sc = new Scanner(System.in);
+
         // 공백이 없는 단일 문자열을 입력받음
-        String a = sc.next();  
-        
+        String a = sc.next();
+
         // Scanner 객체 닫기 (자원 해제)
-        sc.close(); 
-        
+        sc.close();
+
         // 입력받은 문자열을 출력
         System.out.println(a);
     }
