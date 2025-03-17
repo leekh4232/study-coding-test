70d50c5f4e8ac5787242793997490ce54bbab131
이광호 <leekh4232@gmail.com>
2025-03-17 12:55:04 +0900

    [level 0] Title: 특수문자 출력하기, Time: 127.97 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
-
-public class Solution {
-    public static void main(String[] args) {
+public class Solution { 
+    public static void main(String[] args) { 
+        // 특수문자를 포함한 문자열을 출력
+        // 백슬래시(\), 작은따옴표('), 큰따옴표(")는 이스케이프 문자로 처리해야 함
         System.out.print("!@#$%^&*(\\'\"<>?:;");
     }
 }
