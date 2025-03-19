40bcb6dcb513d5e6647607d88b782e858852e430
이광호 <leekh4232@gmail.com>
2025-03-19 11:30:37 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,14 +1,23 @@
 class Solution {
     public int solution(int angle) {
-        if (angle < 90) return 1; // 예각
-        if (angle == 90) return 2; // 직각
-        if (angle < 180) return 3; // 둔각
-        return 4; // 평각
+        int answer = 0;
+
+        if (angle < 90) {
+            answer = 1; // 예각
+        } else if (angle == 90) {
+            answer = 2; // 직각
+        } else if (angle < 180) {
+            answer = 3; // 둔각
+        } else {
+            answer = 4; // 평각
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        
+
         // 테스트 예제 실행
         System.out.println(s.solution(70));  // 1
         System.out.println(s.solution(91));  // 3
