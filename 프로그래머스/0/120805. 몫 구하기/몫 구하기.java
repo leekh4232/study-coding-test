c7e7e702c62773eafd9ce3abbc6cad784b50d9fa
이광호 <leekh4232@gmail.com>
2025-03-26 02:24:16 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -1,11 +1,14 @@
+// Solution 클래스 정의
 public class Solution {
-    public int solution(int num1, int num2) {
+    // solution 메서드는 두 정수를 입력받아 num1을 num2로 나눈 몫을 반환함
+    public static int solution(int num1, int num2) {
+        // 정수 나눗셈 연산을 수행하여 결과를 반환
         return num1 / num2;
     }
 
+    // main 메서드는 프로그램 실행의 시작점이며 테스트를 위해 사용됨
     public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.solution(10, 5));
-        System.out.println(s.solution(7, 2));
+        System.out.println(solution(10, 5));
+        System.out.println(solution(7, 2));
     }
 }
