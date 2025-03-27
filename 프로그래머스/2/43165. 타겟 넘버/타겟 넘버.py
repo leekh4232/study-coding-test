113e2e71c3a5d5851c6393be7ba26602ba0d31ef
이광호 <leekh4232@gmail.com>
2025-03-27 19:24:31 +0900

    [level 2] Title: 타겟 넘버, Time: 463.39 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,3 @@
-def dfs(numbers, target, current, idx):
-    if idx == len(numbers):
-        return 1 if current == target else 0
-
-    count = 0
-    count += dfs(numbers, target, current + numbers[idx], idx + 1)
-    count += dfs(numbers, target, current - numbers[idx], idx + 1)
-    return count
-
 def solution(numbers, target):
-    return dfs(numbers, target, 0, 0)
-
-if __name__ == "__main__":
-    print(solution([1, 1, 1, 1, 1], 3)) # 5
-    print(solution([4, 1, 2, 1], 4))    # 2
+    answer = 0
+    return answer
