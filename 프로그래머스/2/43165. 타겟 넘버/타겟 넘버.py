55f0804aa9893cab11c8c78234034bef05d1644f
이광호 <leekh4232@gmail.com>
2025-03-27 19:25:23 +0900

    [level 2] Title: 타겟 넘버, Time: 557.16 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,19 @@
 def solution(numbers, target):
-    answer = 0
-    return answer
+    stack = [(0, 0)]  # (현재 합, 인덱스)
+    count = 0
+
+    while stack:
+        current, idx = stack.pop()
+
+        if idx == len(numbers):
+            if current == target:
+                count += 1
+        else:
+            stack.append((current + numbers[idx], idx + 1))
+            stack.append((current - numbers[idx], idx + 1))
+
+    return count
+
+if __name__ == "__main__":
+    print(solution([1, 1, 1, 1, 1], 3)) # 5
+    print(solution([4, 1, 2, 1], 4))    # 2
