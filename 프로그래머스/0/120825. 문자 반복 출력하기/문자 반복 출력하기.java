916a9a23fb8ed9cd2d80822860a78cb7700f6c58
이광호 <leekh4232@gmail.com>
2025-04-01 15:40:07 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 12.98 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,23 +1,21 @@
-class Solution {
-    public String solution(String myString, int n) {
-        String result = "";  // 결과를 저장할 문자열 초기화
+public class Solution {
+    public static String solution(String my_string, int n) {
+        String answer = "";
 
-        // myString의 각 문자를 순회
-        for (int i = 0; i < myString.length(); i++) {
-            String currentChar = myString.substring(i, i + 1);  // 현재 문자 추출
-            
-            // 현재 문자를 n번 반복하여 추가
-            for (int j = 0; j < n; j++) {
-                result += currentChar;
+        // 각 글자를 배열로 분리
+        char[] cArr = my_string.toCharArray();
+
+        // 문자열의 각 문자를 순회하며 변환
+        for (char c : cArr) {
+            for (int i=0; i<n; i++) {
+                answer += c;
             }
         }
 
-        return result;  // 최종 변환된 문자열 반환
+        return answer;
     }
 
-    // 테스트 코드
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.solution("hello", 3));  // "hhheeellllllooo"
+        System.out.println(solution("hello", 3));       // "hhheeellllllooo"
     }
 }
