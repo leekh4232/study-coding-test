7386f24b4398bcc8413f1d7a1362f067142956ed
이광호 <leekh4232@gmail.com>
2025-04-01 15:36:18 +0900

    [level 0] Title: 대문자와 소문자, Time: 15.55 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,23 +1,27 @@
-class Solution {
-    public String solution(String myString) {
-        String result = "";  // 변환된 문자열을 저장할 변수
+public class Solution {
+    public static String solution(String my_string) {
+        String answer = "";
+
+        // 각 글자를 배열로 분리
+        char[] cArr = my_string.toCharArray();
 
         // 문자열의 각 문자를 순회하며 변환
-        for (char c : myString.toCharArray()) {
-            if (Character.isUpperCase(c)) {  // 현재 문자가 대문자인 경우
-                result += Character.toLowerCase(c);  // 소문자로 변환하여 추가
-            } else {  // 현재 문자가 소문자인 경우
-                result += Character.toUpperCase(c);  // 대문자로 변환하여 추가
+        for (char c : cArr) {
+            int k = (int) c;
+
+            // 아스키코드에 의한 대문자
+            if (k >= 65 && k <= 96) {
+                answer += (char)(k + 32);
+            } else {
+                answer += (char)(k - 32);
             }
         }
 
-        return result;  // 변환된 문자열 반환
+        return answer;
     }
 
-    // 테스트 코드
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.solution("cccCCC"));       // CCCccc
-        System.out.println(sol.solution("abCdEfghIJ"));   // ABcDeFGHij
+        System.out.println(solution("cccCCC"));       // CCCccc
+        System.out.println(solution("abCdEfghIJ"));   // ABcDeFGHij
     }
 }
