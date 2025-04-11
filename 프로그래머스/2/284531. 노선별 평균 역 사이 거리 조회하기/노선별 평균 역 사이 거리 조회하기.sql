573400952d2a4bb4b6cf6acddb29b4c7b761b044
이광호 <leekh4232@gmail.com>
2025-04-11 16:53:02 +0900

    [level 2] Title: 노선별 평균 역 사이 거리 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284531. 노선별 평균 역 사이 거리 조회하기/노선별 평균 역 사이 거리 조회하기.sql
+++ 프로그래머스/2/284531. 노선별 평균 역 사이 거리 조회하기/노선별 평균 역 사이 거리 조회하기.sql
@@ -4,5 +4,6 @@ SELECT
     CONCAT(ROUND(AVG(D_BETWEEN_DIST), 2), 'km') AS AVERAGE_DISTANCE
 FROM SUBWAY_DISTANCE
 GROUP BY ROUTE
--- TOTAL_DISTANCE를 사용할 경우 CONCAT함수의 결과로 문자열로 변환되기 때문에 정렬 기준이 달라진다.
+-- TOTAL_DISTANCE를 사용할 경우 CONCAT함수의 결과인
+-- 문자열로 변환되기 때문에 정렬 기준이 달라진다.
 ORDER BY ROUND(SUM(D_BETWEEN_DIST), 1) DESC;
