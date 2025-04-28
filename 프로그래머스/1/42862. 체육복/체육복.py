ce5233a956e53cdd864554fffae987d7e8729062
이광호 <leekh4232@gmail.com>
2025-04-29 07:59:30 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -11,10 +11,10 @@ def solution(n, lost, reserve):
 
     # 남은 reserve 리스트를 순회하면서 체육복 빌려주기
     for i in reserve:
-        if i-1 in lost:        # 앞번호 학생이 체육복이 없는 경우
-            lost.remove(i-1)   # 앞번호 학생에게 체육복을 빌려주고 lost에서 제거
-        elif i+1 in lost:      # 앞번호 학생이 없고 뒷번호 학생이 체육복이 없는 경우
-            lost.remove(i+1)   # 뒷번호 학생에게 체육복을 빌려주고 lost에서 제거
+        if i-1 in lost:       # 앞번호 학생이 체육복이 없는 경우
+            lost.remove(i-1)  # 앞번호 학생에게 체육복을 빌려주고 lost에서 제거
+        elif i+1 in lost:     # 앞번호 학생이 없고 뒷번호 학생이 체육복이 없는 경우
+            lost.remove(i+1)  # 뒷번호 학생에게 체육복을 빌려주고 lost에서 제거
 
     # 전체 학생 수에서 여전히 체육복이 없는 학생 수를 빼서 결과 반환
     return n - len(lost)
