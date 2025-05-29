6857a7aa9d67c6dd4fadb841c6c7c6f829e857e8
이광호 <leekh4232@gmail.com>
2025-05-30 00:37:43 +0900

    /

--- Solution.js
+++ Solution.js
@@ -1,37 +1,79 @@
-function solution(N) {
-    if (N.length <= 1) {
-        return N;
+class PriorityQueue {
+    constructor() {
+        this.heap = [];
     }
 
-    let mid = Math.floor(N.length / 2);
-    let left = N.slice(0, mid);
-    let right = N.slice(mid);
+    // 요소 추가: [priority, value] 형태
+    push(priority, value) {
+        const node = { priority, value };
+        this.heap.push(node);
+        this.#bubbleUp();
+    }
 
-    let sortedLeft = solution(left);
-    let sortedRight = solution(right);
+    // 우선순위가 가장 높은 요소 꺼내기
+    pop() {
+        if (this.size() === 0) return null;
+        const top = this.heap[0];
+        const last = this.heap.pop();
 
-    return merge(sortedLeft, sortedRight);
-}
-
-function merge(left, right) {
-    let sortedList = [];
-    let i = 0, j = 0;
-
-    while (i < left.length && j < right.length) {
-        if (left[i] < right[j]) {
-            sortedList.push(left[i++]);
-        } else {
-            sortedList.push(right[j++]);
+        if (this.size() !== 0) {
+            this.heap[0] = last;
+            this.#sinkDown();
         }
+
+        return top;
     }
 
-    while (i < left.length) {
-        sortedList.push(left[i++]);
+    // 최상위 요소 확인
+    peek() {
+        return this.heap[0] || null;
     }
 
-    while (j < right.length) {
-        sortedList.push(right[j++]);
+    // 큐 크기 확인
+    size() {
+        return this.heap.length;
     }
 
-    return sortedList;
+    // 내부 정렬: 위로 올림
+    #bubbleUp() {
+        let index = this.heap.length - 1;
+        const node = this.heap[index];
+
+        while (index > 0) {
+            const parentIdx = Math.floor((index - 1) / 2);
+            if (this.heap[parentIdx].priority <= node.priority) break;
+
+            this.heap[index] = this.heap[parentIdx];
+            index = parentIdx;
+        }
+
+        this.heap[index] = node;
+    }
+
+    // 내부 정렬: 아래로 내림
+    #sinkDown() {
+        let index = 0;
+        const length = this.heap.length;
+        const node = this.heap[0];
+
+        while (true) {
+            const leftIdx = 2 * index + 1;
+            const rightIdx = 2 * index + 2;
+            let smallest = index;
+
+            if (leftIdx < length && this.heap[leftIdx].priority < this.heap[smallest].priority) {
+                smallest = leftIdx;
+            }
+            if (rightIdx < length && this.heap[rightIdx].priority < this.heap[smallest].priority) {
+                smallest = rightIdx;
+            }
+
+            if (smallest === index) break;
+
+            this.heap[index] = this.heap[smallest];
+            index = smallest;
+        }
+
+        this.heap[index] = node;
+    }
 }
