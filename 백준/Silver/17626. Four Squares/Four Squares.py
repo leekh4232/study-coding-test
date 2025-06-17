5acda7e2c04d9727402b20c9f703eee387d36bc0
이광호 <leekh4232@gmail.com>
2025-06-17 12:55:32 +0900

    [Silver III] Title: Four Squares, Time: 128 ms, Memory: 110732 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,35 +1,11 @@
-import math
-import sys
-input = sys.stdin.readline
-
 def solution(n):
-    # 1) 1개 제곱수
-    root = int(math.isqrt(n))
-    if root * root == n:
-        return 1
-
-    # 2) 2개 제곱수: n = i^2 + j^2
-    for i in range(1, root + 1):
-        rem = n - i * i
-        j = int(math.isqrt(rem))
-        if j * j == rem:
-            return 2
-
-    # 3) 3개 제곱수: n = i^2 + j^2 + k^2
-    # 이미 1,2개 경우가 없다는 가정
-    squares = [i*i for i in range(1, root+1)]
-    m = len(squares)
-    for i in range(m):
-        for j in range(i, m):
-            rem = n - squares[i] - squares[j]
-            if rem < 0:
-                break
-            k = int(math.isqrt(rem))
-            if k*k == rem:
-                return 3
-
-    # 4) 그 외는 무조건 4개 (Lagrange 정리)
-    return 4
+    dp = [0] + [float('inf')] * n
+    for i in range(1, n+1):
+        j = 1
+        while j * j <= i:
+            dp[i] = min(dp[i], dp[i - j*j] + 1)
+            j += 1
+    return dp[n]
 
 if __name__ == "__main__":
     n = int(input().strip())
