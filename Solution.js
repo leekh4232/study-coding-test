05fe6dcc1e5cf525b8bf4a2f4fee15d372236e65
이광호 <leekh4232@gmail.com>
2025-06-22 08:15:40 +0900

    leekh

--- Solution.js
+++ Solution.js
@@ -1,35 +1,13 @@
-// 전화번호 목록에 접두어 관계가 있는지 확인하는 함수
-function solution(phoneBook) {
-    // 전화번호를 키로 저장할 해시맵 객체 생성
-    const hashMap = {};
+import Heap from './Heap.js';
 
-    // 모든 전화번호를 해시맵에 저장 (값은 의미 없음)
-    for (const nums of phoneBook) {
-        hashMap[nums] = true;
-    }
+// ✅ Python의 heapify 동작과 같은 코드
+const data = [5, 1, 3, 2, 8, 7];
+const heap = new Heap();
 
-    // 각 전화번호마다 접두어 검사 수행
-    for (const nums of phoneBook) {
-        // 접두어를 만들기 위한 문자열 초기화
-        let arr = "";
+// 모든 요소를 힙에 삽입 (heapify와 동일한 효과)
+data.forEach(num => heap.push(num));
 
-        // 전화번호의 각 숫자를 앞에서부터 더해 나가며 접두어 생성
-        for (const num of nums) {
-            arr += num;
-
-            // 접두어가 해시맵에 존재하고, 자기 자신은 아닐 때
-            if (arr in hashMap && arr !== nums) {
-                // 접두어 관계가 존재하므로 즉시 false 반환
-                return false;
-            }
-        }
-    }
-
-    // 모든 전화번호 검사 결과 접두어 관계가 없으므로 true 반환
-    return true;
+for (let i = 0; i < data.length; i++) {
+    // 힙에서 최상위 요소를 제거하고 출력
+    console.log(heap.pop());
 }
-
-// 테스트 케이스 실행
-console.log(solution(["119", "97674223", "1195524421"])); // False
-console.log(solution(["123", "456", "789"])); // True
-console.log(solution(["12", "123", "1235", "567", "88"])); // False
