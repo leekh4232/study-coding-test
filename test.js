2d5e6daaa62727c12c14935c5d15d6efea6fa3a6
이광호 <leekh4232@gmail.com>
2025-06-25 00:54:36 +0900

    leekh

--- test.js
+++ test.js
@@ -1,22 +1,38 @@
-import PriorityQueue from "./PriorityQueue.js";
+function solution(correct, submission) {
+    const pos = {};		// 정답 순서 맵핑: 해전 이름 → 정답 인덱스
+    correct.forEach((name, index) => {
+        pos[name] = index;
+    });
 
-function solution(xlist) {
-    let myQueue = new PriorityQueue();      // 우선순위 큐 생성
-    let answer = [];                        // 결과를 저장할 배열
+    const n = correct.length;
+    let point = 0;
 
-    for (let x of xlist) {                  // 입력 배열을 순회
-        if (x !== 0) {                      // 숫자가 0이 아니라면?
-            myQueue.put([Math.abs(x), x]);  // 절댓값을 기준으로 큐에 숫자를 추가
-        } else {                            // 숫자가 0이면
-            if (myQueue.size() == 0) {      // 큐가 비어있으면
-                answer.push(0);             // 결과 배열에 0 추가
-            } else {                        // 큐가 비어있지 않으면
-                let temp = myQueue.get();   // 큐에서 가장 작은 값을 꺼냄
-                answer.push(temp[1]);       // 해당 값을 결과 배열에 추가
+    // 모든 i < j 쌍을 순회하며 순서쌍 점수 계산
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (pos[submission[i]] < pos[submission[j]]) {
+                point++;
             }
         }
     }
-    return answer;                          // 결과 배열을 반환
+
+    const total = (n * (n - 1)) / 2;
+
+    return `${point}/${total}`; // "point/total" 문자열로 결과 반환
 }
-// -> [-1, 1, 0, -1, -1, 1, 1, -2, 2, 0]
-console.log(solution([1, -1, 0, 0, 0, 1, 1, -1, -1, 2, -2, 0, 0, 0, 0, 0, 0, 0]));
+
+// 테스트 실행
+console.log(solution(
+    ['okpo', 'sacheon', 'hansan', 'myeongnyang', 'noryang'],
+    ['sacheon', 'hansan', 'myeongnyang', 'noryang', 'okpo']
+)); // "6/10"
+
+console.log(solution(
+    ['alpha', 'beta', 'gamma'],
+    ['alpha', 'gamma', 'beta']
+)); // "2/3"
+
+console.log(solution(
+    ['naboo', 'geonosis', 'yavin', 'hoth', 'endor'],
+    ['geonosis', 'yavin', 'hoth', 'endor', 'naboo']
+)); // "6/10"
