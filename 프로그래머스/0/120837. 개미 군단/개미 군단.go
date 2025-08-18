76ff5089bca8ce5bbad830ec228afa3b71531fef
이광호 <leekh4232@gmail.com>
2025-08-18 13:10:35 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.go
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.go
@@ -1,7 +1,7 @@
 func solution(hp int) int {
-    var reminder int = hp % 5;
-    var general int = (hp - reminder) / 5;
-    var worker int = reminder % 3;
-    var soldier int = (reminder - worker) / 3;
-    return general + soldier + worker;
+    var reminder int = hp % 5
+    var general int = (hp - reminder) / 5
+    var worker int = reminder % 3
+    var soldier int = (reminder - worker) / 3
+    return general + soldier + worker
 }
