e4acf90ce19d57068c9d8813d442937b1b5d64f9
이광호 <leekh4232@gmail.com>
2025-08-18 13:10:19 +0900

    [level 0] Title: 종이 자르기, Time: 0.00 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.go
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.go
@@ -1,10 +1,10 @@
 func solution(M int, N int) int {
     // 세로 방향으로 (M - 1)번 자르면 M줄 생성
-    var verticalCuts int = M - 1;
+    var verticalCuts int = M - 1
 
     // 각 세로 조각마다 가로 방향으로 (N - 1)번 자름 → M줄 모두 잘라야 하므로 M * (N - 1)
-    var horizontalCuts int = M * (N - 1);
+    var horizontalCuts int = M * (N - 1)
 
     // 총 가위질 횟수는 세로 절단 + 가로 절단
-    return verticalCuts + horizontalCuts;
+    return verticalCuts + horizontalCuts
 }
