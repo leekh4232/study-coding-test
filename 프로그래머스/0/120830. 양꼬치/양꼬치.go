9bbaa89ada86e05c8d928314be7b10f7e480ab44
이광호 <leekh4232@gmail.com>
2025-08-18 13:10:52 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.go
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.go
@@ -1,19 +1,19 @@
 func solution(n int, k int) int {
     // 서비스로 제공되는 음료수 개수 (10인분당 1개)
-    var freeDrinks int = n / 10;
+    var freeDrinks int = n / 10
 
     // 실제 결제해야 할 음료수 개수 (전체 주문 개수 - 서비스 개수)
-    var paidDrinks int = k - freeDrinks;
+    var paidDrinks int = k - freeDrinks
 
     // 양꼬치 총 가격 계산
-    var totalLambCost int = 12000 * n;
+    var totalLambCost int = 12000 * n
 
     // 음료수 총 가격 계산 (서비스는 제외)
-    var totalDrinkCost int = 2000 * paidDrinks;
+    var totalDrinkCost int = 2000 * paidDrinks
 
     // 총 결제 금액 계산
-    var totalCost int = totalLambCost + totalDrinkCost;
+    var totalCost int = totalLambCost + totalDrinkCost
 
     // 최종 결과 반환
-    return totalCost;
+    return totalCost
 }
