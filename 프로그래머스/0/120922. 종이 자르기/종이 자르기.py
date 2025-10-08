2b7fc7a9062ecaa36f6248b3fe9c0ba33c5e8f4d
이광호 <leekh4232@gmail.com>
2025-10-08 23:03:44 +0900

    [level 0] Title: 종이 자르기, Time: 0.00 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.py
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.py
@@ -1,7 +1,15 @@
 def solution(M, N):
-    answer = (M-1) + M * (N-1) 
-    return answer
+    # 세로 방향으로 (M - 1)번 자르면 M줄 생성
+    verticalCuts = M - 1
 
-print(solution(2, 2))       # 결과값 : 3
-print(solution(2, 5))       # 결과값 : 9
-print(solution(1, 1))       # 결과값 : 0
+    # 각 세로 조각마다 가로 방향으로 (N - 1)번 자름 → M줄 모두 잘라야 하므로 M * (N - 1)
+    horizontalCuts = M * (N - 1)
+
+    # 총 가위질 횟수는 세로 절단 + 가로 절단
+    return verticalCuts + horizontalCuts
+
+
+# 테스트
+print(solution(2, 2)) # 3
+print(solution(2, 5)) # 9
+print(solution(1, 1)) # 0
